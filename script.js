d8b5ffa9335dfb35d8065b1aff541140c2eb8330
Divyansh Kumar <118956824+divyansh14kr@users.noreply.github.com>
2023-08-11 22:03:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// function calculate() {
+//     try{
+//         const height = document.getElementById("height");
+//         const weight = document.getElementById("weight");
+//         const bmi =  eval(height/(weight*weight));
+//         if (bmi <18){
+//             document.getElementById("display").value = 'Underweight';
+//         }
+//         if (bmi >18 && bmi <24.9){
+//             document.getElementById("display").value = 'Normal Weight';
+//         }
+//         if(bmi >24.9 && bmi < 29.9){
+//             document.getElementById("display").value = 'Overweight';
+//         }
+//         if(bmi > 29.9 && bmi< 34.9){
+//             document.getElementById("display").value = 'Obesity Class-Ⅰ';
+//         }
+//         if(bmi>34.9 && bmi< 39.9){
+//             document.getElementById("display").value = 'Obesity Class-Ⅱ';
+//         }
+//         if(bmi>39.9){
+//             document.getElementById("display").value = 'Obesity Class-Ⅲ';
+//         }
+//     }catch(error){
+//         document.getElementById("display").value = 'Error';
+//     }
+// }
+function calculate() {
+    try {
+        const height = parseFloat(document.getElementById("height").value);
+        const weight = parseFloat(document.getElementById("weight").value);
+        const bmi = height / (weight * weight);
+
+        if (bmi < 18) {
+            document.getElementById("display").value = 'Underweight';
+        } else if (bmi < 24.9) {
+            document.getElementById("display").value = 'Normal Weight';
+        } else if (bmi < 29.9) {
+            document.getElementById("display").value = 'Overweight';
+        } else if (bmi < 34.9) {
+            document.getElementById("display").value = 'Obesity Class-Ⅰ';
+        } else if (bmi < 39.9) {
+            document.getElementById("display").value = 'Obesity Class-Ⅱ';
+        } else {
+            document.getElementById("display").value = 'Obesity Class-Ⅲ';
+        }
+    } catch (error) {
+        document.getElementById("display").value = 'Error';
+    }
+}
